f954377c663b8f29528839f4e505b47f655aa82a
pitersizovdev <pitersizov@gmail.com>
2024-07-04 17:54:17 +0300

    upd: slides_api

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import "./App.css";
 import { ImageSlider } from "./components/ImageSlider/ImageSlider";
 
 const images = [
-  "https://via.placeholder.com/600x400/FF0000/FFFFFF?text=Slide+1",
-  "https://via.placeholder.com/600x400/00FF00/FFFFFF?text=Slide+2",
-  "https://via.placeholder.com/600x400/0000FF/FFFFFF?text=Slide+3",
+  "https://placehold.co/600x400?text=Slide+1",
+  "https://placehold.co/600x400?text=Slide+2",
+  "https://placehold.co/600x400?text=Slide+3",
 ];
 
 const App: React.FC = () => (
